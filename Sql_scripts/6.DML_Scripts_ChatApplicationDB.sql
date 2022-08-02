2e6fd04c230dcfe9e27a5d156ae34217c59b9b7d
Shubham <155.shubham@gmail.com>
2022-08-03 01:45:22 +0530

    Update 6.DML_Scripts_ChatApplicationDB.sql

--- Sql_scripts/6.DML_Scripts_ChatApplicationDB.sql
+++ Sql_scripts/6.DML_Scripts_ChatApplicationDB.sql
@@ -110,105 +110,88 @@ VALUES
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(20, 'juniorsupport1@cc.com', '111122230', 'test', GETDATE(), 'Junior', 1, NULL, 1, 'TeamA');
+	(20, 'juniorsupport1@cc.com', '111122230', 'test', GETDATE(), 'Junior', 1, NULL, 0, 'TeamA');
 
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(21, 'juniorsupport2@cc.com', '111122231', 'test', GETDATE(), 'Junior', 1, NULL, 1, 'TeamB');
+	(21, 'juniorsupport2@cc.com', '111122231', 'test', GETDATE(), 'Junior', 1, NULL, 0, 'TeamB');
 
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(22, 'juniorsupport3@cc.com', '111122231', 'test', GETDATE(), 'Junior', 1, NULL, 1, 'TeamB');
+	(22, 'juniorsupport3@cc.com', '111122231', 'test', GETDATE(), 'Junior', 1, NULL, 0, 'TeamB');
 
 -- Mid Levels
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(24, 'midlevelsupport1@cc.com', '111122230', 'test', GETDATE(), 'MidLevel', 1, NULL, 1, 'TeamA');
+	(24, 'midlevelsupport1@cc.com', '111122230', 'test', GETDATE(), 'MidLevel', 1, NULL, 0, 'TeamA');
 
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(25, 'midlevelsupport2@cc.com', '111122230', 'test', GETDATE(), 'Midlevel', 1, NULL, 1, 'TeamA');
+	(25, 'midlevelsupport2@cc.com', '111122230', 'test', GETDATE(), 'Midlevel', 1, NULL, 0, 'TeamA');
 
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(26, 'midlevelsupport3@cc.com', '111122231', 'test', GETDATE(), 'Midlevel', 1, NULL, 1, 'TeamB');
+	(26, 'midlevelsupport3@cc.com', '111122231', 'test', GETDATE(), 'Midlevel', 1, NULL, 0, 'TeamB');
 
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(27, 'midlevelsupport4@cc.com', '111122232', 'test', GETDATE(), 'Midlevel', 1, NULL, 1, 'TeamC');
+	(27, 'midlevelsupport4@cc.com', '111122232', 'test', GETDATE(), 'Midlevel', 1, NULL, 0, 'TeamC');
 
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(28, 'midlevelsupport5@cc.com', '111122232', 'test', GETDATE(), 'Midlevel', 1, NULL, 1, 'TeamC');
+	(28, 'midlevelsupport5@cc.com', '111122232', 'test', GETDATE(), 'Midlevel', 1, NULL, 0, 'TeamC');
 
 -- Senior
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(29, 'seniorsupport1@cc.com', '111122231', 'test', GETDATE(), 'Senior', 1, NULL, 1, 'TeamB');
+	(29, 'seniorsupport1@cc.com', '111122231', 'test', GETDATE(), 'Senior', 1, NULL, 0, 'TeamB');
 
 -- Team Lead
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(31, 'teamleadsupport1@cc.com', '111122230', 'test', GETDATE(), 'TeamLead', 1, NULL, 1, 'TeamA');
+	(31, 'teamleadsupport1@cc.com', '111122230', 'test', GETDATE(), 'TeamLead', 1, NULL, 0, 'TeamA');
 
 -- Team Overflow
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(33, 'juniorsupport4@cc.com', '111122233', 'test', GETDATE(), 'Junior', 1, NULL, 1, 'TeamOverflow');
+	(33, 'juniorsupport4@cc.com', '111122233', 'test', GETDATE(), 'Junior', 1, NULL, 0, 'TeamOverflow');
 
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(34, 'juniorsupport5@cc.com', '111122233', 'test', GETDATE(), 'Junior', 1, NULL, 1, 'TeamOverflow');
+	(34, 'juniorsupport5@cc.com', '111122233', 'test', GETDATE(), 'Junior', 1, NULL, 0, 'TeamOverflow');
 
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(35, 'juniorsupport6@cc.com', '111122233', 'test', GETDATE(), 'Junior', 1, NULL, 1, 'TeamOverflow');
+	(35, 'juniorsupport6@cc.com', '111122233', 'test', GETDATE(), 'Junior', 1, NULL, 0, 'TeamOverflow');
 
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(36, 'juniorsupport7@cc.com', '111122233', 'test', GETDATE(), 'Junior', 1, NULL, 1, 'TeamOverflow');
+	(36, 'juniorsupport7@cc.com', '111122233', 'test', GETDATE(), 'Junior', 1, NULL, 0, 'TeamOverflow');
 
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(37, 'juniorsupport8@cc.com', '111122233', 'test', GETDATE(), 'Junior', 1, NULL, 1, 'TeamOverflow');
+	(37, 'juniorsupport8@cc.com', '111122233', 'test', GETDATE(), 'Junior', 1, NULL, 0, 'TeamOverflow');
 
 INSERT INTO dbo.UserInfo
 	(userId, email, mobile, [password], dob, [role], IsEmployee, LoginTime, FreeOrBusy, TeamName)
 VALUES
-	(38, 'juniorsupport9@cc.com', '111122233', 'test', GETDATE(), 'Junior', 1, NULL, 1, 'TeamOverflow');
-
---INSERT INTO dbo.SupportAndCustomerMapping
---	(supportId,customerId)
---VALUES
---	(20, 11);
+	(38, 'juniorsupport9@cc.com', '111122233', 'test', GETDATE(), 'Junior', 1, NULL, 0, 'TeamOverflow');
 
 select * from dbo.UserInfo;
-
---select * from dbo.SupportAndCustomerMapping;
-
-
---SELECT CustomerList.Customer, UI2.email AS Agent
---FROM 
---	(SELECT		UI.email As Customer, SACM.supportId
---	FROM		[ChatApplicationDB].[dbo].[SupportAndCustomerMapping]	AS SACM
---	INNER JOIN	[ChatApplicationDB].[dbo].[UserInfo]					AS	UI
---	ON			UI.userId	= SACM.customerId) AS CustomerList
---INNER JOIN	[ChatApplicationDB].[dbo].[UserInfo] AS UI2
---ON			UI2.userId = CustomerList.supportId
 
 GO
 
